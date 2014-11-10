f6201df7897c8b237ecb601bff3967929f7eee50
moonwitch <kelly.crabbe@gmail.com>
2014-11-10 13:06:05 +0100

    removing nameplates fix

--- uitweaks.lua
+++ uitweaks.lua
@@ -1,28 +1,13 @@
-local _, Augmento = ...
 ------------------------------------------------------------------------
--- Toggle nameplates on while in combat
+-- Setting up the local scope so we can work in modules
+------------------------------------------------------------------------
+local _, Augmento = ...
 
-function Augmento.PLAYER_REGEN_DISABLED(event)
-  --self:Debug(event)
-  if db.showNameplatesInCombat then
-    SetCVar("nameplateShowEnemies", 1)
-  end
-  if summonPending then
-    self:StopSummonDelayTimer()
-  end
 function Augmento.ApplyUITweaks()
   -- Fix Macro font
   SystemFont_Shadow_Small:SetFont(STANDARD_TEXT_FONT, 12)
 end
 
-function Augmento.PLAYER_REGEN_ENABLED(event)
-  --self:Debug(event)
-  if db.showNameplatesInCombat then
-    SetCVar("nameplateShowEnemies", 0)
-  end
-  if summonPending then
-    self:StartSummonDelayTimer()
-  end
 -- Slash Commands that shouldn't have been forgotten
 SlashCmdList['RELOAD_UI'] = function() ReloadUI() end
 SLASH_RELOAD_UI1 = '/rl'
