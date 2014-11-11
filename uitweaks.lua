682114bd9d760c6870468b1d295a83e9a62625c2
moonwitch <kelly.crabbe@gmail.com>
2014-11-11 04:54:53 +0100

    Correctly add Selling trash and auto repairing with chat feedback for both

--- uitweaks.lua
+++ uitweaks.lua
@@ -4,10 +4,11 @@
 local _, Augmento = ...
 
 function Augmento.ApplyUITweaks()
-  -- Fix Macro font
-  SystemFont_Shadow_Small:SetFont(STANDARD_TEXT_FONT, 12)
+   -- Fix Macro font
+   SystemFont_Shadow_Small:SetFont(STANDARD_TEXT_FONT, 12)
 end
 
+------------------------------------------------------------------------
 -- Slash Commands that shouldn't have been forgotten
 SlashCmdList['RELOAD_UI'] = function() ReloadUI() end
 SLASH_RELOAD_UI1 = '/rl'
@@ -24,10 +25,65 @@ end
 ------------------------------------------------------------------------
 -- Toggle nameplates on while in combat
 function Augmento.PLAYER_REGEN_DISABLED(...)
-  SetCVar("nameplateShowEnemies", 1)
-
+   SetCVar("nameplateShowEnemies", 1)
 end
 
 function Augmento.PLAYER_REGEN_ENABLED(...)
-  SetCVar("nameplateShowEnemies", 0)
+   SetCVar("nameplateShowEnemies", 0)
+end
+
+------------------------------------------------------------------------
+-- Sell your shit
+
+-- Parse Gold/Silver/Copper
+local function money_to_string(value)
+  if value == 0 then return nil end
+
+  local gold = math.floor(value / 10000)
+  local silver = mod(math.floor(value / 100), 100)
+  local copper = mod(value, 100)
+
+  return string.format("|cffffd700%i|r.|cffc7c7cf%02i|r.|cffeda55f%02i|r", gold, silver, copper)
+end
+
+function Augmento.MERCHANT_SHOW(...)
+   -- Auto-Repair from guild without shiftkey, if shiftkey is down, don't repair at all.
+   if not CanMerchantRepair() or IsShiftKeyDown() then
+      return
+   else
+      if (IsInGuild() and CanGuildBankRepair() and not IsShiftKeyDown()) then -- can we repair from this dude?
+         RepairAllItems(CanGuildBankRepair() and GetGuildBankWithdrawMoney() >= GetRepairAllCost())
+         if GetRepairAllCost() > 0 then
+            print('Guild repaired for : '.. money_to_string(GetRepairAllCost()))
+         end
+      else
+         RepairAllItems()
+         if GetRepairAllCost() > 0 then
+            print('Repaired for : '.. money_to_string(GetRepairAllCost()))
+         end
+      end
+   end
+
+   -- sell off junk
+   local bag, slot
+   local valueSold = 0, 0
+
+   for bag = 0, 4 do
+      for slot = 0, GetContainerNumSlots(bag) do
+         local _, count, _, _, _, _, link = GetContainerItemInfo(bag, slot)
+         if link then
+            local _, _, quality, _, _, _, _, _, _, _, vendorPrice = GetItemInfo(link)
+
+            if quality == 0 and vendorPrice > 0 then
+               ShowContainerSellCursor(bag, slot)
+               UseContainerItem(bag, slot)
+               valueSold = valueSold + vendorPrice * count
+            end
+         end
+      end
+   end
+
+   if valueSold > 0 then
+      print('Sold Junk : ', money_to_string(valueSold))
+   end
 end
