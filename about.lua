fc77d96592702fe89e8b0b6cebc3a3fd0e48c596
moonwitch <kelly.crabbe@gmail.com>
2015-01-04 16:46:44 +0100

    fixed options

--- about.lua
+++ about.lua
@@ -1,66 +1,69 @@
 ------------------------------------------------------------------------
 -- Namespaceing
 ------------------------------------------------------------------------
-local _, Augmento = ...
+-- local _, Augmento = ...
+local addonName = ...
 
 local description = "Augmento is my own setup. I started out with a hacked version of oBindings by haste, it evolved into something more - and thanks to Phanx I\'ve rewritten it completely to be less rape-style.\nThere are no in game settings; all is done in Lua itself. Should you have downloaded this and installed it - congratulations, you\'ve just erased ALL your keybindings."
 local credits = "I\'d like to thank Tekkub and Phanx and haste, Tekkub unbeknowingly inspired me, Phanx is just awesome, her patience with me is endless. Also; cats."
 ------------------------------------------------------------------------
 -- Frame kick off
 ------------------------------------------------------------------------
-local Panel = CreateFrame('Frame', nil)
-Panel.name = Augmento
+local Panel = CreateFrame('Frame', nil, InterfaceOptionsFramePanelContainer)
+Panel.name = addonName
 Panel:Hide()
 
--- Panel.childpanel = CreateFrame( "Frame", nil, Panel)
--- Panel.childpanel.name = ChildPanelName
--- Panel.childpanel:Hide()
+-- Panel.childPanel = CreateFrame( "Frame", nil, Panel)
+-- Panel.childPanel.name = ChildPanelName
+-- Panel.childPanel:Hide()
 --
--- -- Specify childness of this panel
--- Panel.childpanel.parent = Panel.name
+-- -- Specify childness of this Panel
+-- Panel.childPanel.parent = Panel.name
 
 ------------------------------------------------------------------------
 -- GUI widgets
 ------------------------------------------------------------------------
 
 -----------------------------
--- Populating the panel itself (main panel)
+-- Populating the Panel itself (main Panel)
 Panel:SetScript('OnShow', function(self)
   local Title = self:CreateFontString(nil, Panel, 'GameFontNormalLarge')
   Title:SetPoint('TOPLEFT', 16, -16)
-  Title:SetText(Augmento)
+  Title:SetText(addonName)
 
   local Author = self:CreateFontString(nil, Panel, 'GameFontNormal')
   Author:SetPoint('TOPLEFT', Title, 'BOTTOMLEFT', 0, -8)
   Author:SetPoint('RIGHT', -32, 0)
   Author:SetJustifyH('LEFT')
-  Author:SetText(GetAddOnMetadata(Augmento, "Author"))
-  -- self.Author = Author
+  Author:SetText(GetAddOnMetadata(addonName, "Author"))
+  -- self.Panel.Author = Author
 
   local Notes = self:CreateFontString(nil, Panel, 'GameFontNormal')
   Notes:SetPoint("TOPLEFT", Author, "BOTTOMLEFT", 0, 0)
   Notes:SetPoint("RIGHT", Panel, "RIGHT")
   Notes:SetText(description)
 
-  local Credits = self:CreateFontString(nil, Panel, 'GameFontNormal')
-  Credits:SetPoint("TOPLEFT", Notes, "BOTTOMLEFT", 0, 0)
-  Credits:SetPoint("RIGHT", Panel, "RIGHT")
-  Credits:SetText(credits)
+  local creditsField = self:CreateFontString(nil, Panel, 'GameFontNormal')
+  creditsField:SetPoint("TOPLEFT", Notes, "BOTTOMLEFT", 0, 0)
+  creditsField:SetPoint("RIGHT", Panel, "RIGHT")
+  creditsField:SetText(credits)
 
   self:SetScript('OnShow', nil)
 end)
 
 -----------------------------
--- Add the panel to the Interface Options
+-- Add the Panel to the Interface Options
 InterfaceOptions_AddCategory(Panel)
 
 -----------------------------
 -- Add the child to the Interface Options
--- InterfaceOptions_AddCategory(Panel.childpanel)
+-- InterfaceOptions_AddCategory(Panel.childPanel)
 
 -----------------------------
 -- Adding a SlashCommand to open the correct Panel
-SLASH_AUGMENTO1 = '/augmento'
-SlashCmdList["Augmento"] = function()
-   InterfaceOptionsFrame_OpenToCategory(Augmento)
+SLASH_Augmento1 = '/augmento'
+SLASH_Augmento2 = '/aug'
+SlashCmdList[addonName] = function()
+   InterfaceOptionsFrame_OpenToCategory(addonName)
+   InterfaceOptionsFrame_OpenToCategory(addonName)
 end
